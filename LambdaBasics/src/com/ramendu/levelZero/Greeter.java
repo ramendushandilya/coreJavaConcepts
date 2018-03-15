aa2a2becd288ea316ffec967e69a76e1c81dfbf3
ramendushandilya <ramendu9886@yahoo.com>
2018-03-15 21:25:42 +0530

    o

--- LambdaBasics/src/com/ramendu/levelZero/Greeter.java
+++ LambdaBasics/src/com/ramendu/levelZero/Greeter.java
@@ -1,5 +1,6 @@
 package com.ramendu.levelZero;
 
+
 public class Greeter {
 
     public void greet(IGreeting greeting) {
@@ -7,8 +8,34 @@ public class Greeter {
     }
 
     public static void main(String[] args) {
+
+        // Normal Java 7 way
         IGreeting gr = new HelloWorldGreeting();
         Greeter greeter = new Greeter();
         greeter.greet(gr);
+
+        //Using inner class
+        IGreeting innerClass = new IGreeting() {
+            @Override
+            public void perform() {
+                System.out.println("Hello world from inner class");
+            }
+        };
+        greeter.greet(innerClass);
+
+        /**
+         * Interpretation of lambda expression for self understanding
+         * IGreeting interface is the type of the lambda, all lambdas
+         * in Java are coded to interfaces
+         *
+         * () because IGreeting interface only abstract method doesn't
+         * use any parameter, hence to match signature
+         *
+         * -> is lambda sign
+         *
+         * No return types are used since compiler automatically knows the return type
+         */
+        IGreeting lambdaGreeting = () -> System.out.println("Hello world from lambda");
+        greeter.greet(lambdaGreeting);
     }
 }
--- LambdaBasics/src/com/ramendu/levelZero/IGreeting.java
+++ LambdaBasics/src/com/ramendu/levelZero/IGreeting.java
@@ -1,5 +1,19 @@
 package com.ramendu.levelZero;
 
+/**
+ * Lambda is coded to an interface in Java
+ * For an interface to be eligible to be
+ * target interface it must contain only
+ * one abstract method
+ *
+ * To maintain this contract of only one
+ * abstract method we mark the interface
+ * as FunctionalInterface so that when a
+ * developer adds any other method,
+ * compilation error occurs
+ */
+
+@FunctionalInterface
 public interface IGreeting {
 
     public void perform();
